850c1107fc55d2085b276bec51e2c5442f13e41d
CesarGracia97 <cesa.gracia@pucese.edu.ec>
2026-08-25 08:33:11 -0500

    Estructura unificada: Frontend y Backend

--- proyect-pegasus/src/app/features/tools/services/audio-convert.service.ts
+++ proyect-pegasus/src/app/features/tools/services/audio-convert.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'; //a
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
